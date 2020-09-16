3cb714b1764078446d4dab4aaff85a605eb4c9e5
AfrozaIslam <70200297+AfrozaIslam@users.noreply.github.com>
2020-09-17 00:16:47 +0600

    Add files via upload
    clock

---
+++ app.js
@@ -0,0 +1,55 @@
+//alert("janpakhi");
+
+
+/*var num =20;
+num = parseInt(num);
+console.log(typeof(num));*/
+
+/*var tipu=5;
+    azad=4;
+    hassan=3;
+    sojib=4;
+alert(tipu+azad+hassan+sojib);*/
+
+/*var x= 1;
+
+while(x<100){
+document.write("</br>I Love You ="+x);
+x++;
+}
+*/
+/*for(var i=1; 1<=10; i=i+1){
+  document.write(" "+1) 
+}*/
+/*var i= 1;
+do {
+    document.write(" "+i);
+    i++;
+}
+while(i<=26);*/
+function clock(){
+var hours.getElementById('hours');
+
+var minutes.getElementById('minutes');
+var seconds.getElementById('seconds');
+
+var h =new date().getHours();
+var m =new date().getMinutes();
+var s =new date().getSeconds();
+
+
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML =s;
+ }
+var interval = setInterval(clock,1000);
+
+
+
+
+
+
+
+
+
+
